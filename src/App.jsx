b1d3d50794a31200689252c14ab1e35bf7e3fd6c
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2023-12-14 09:39:29 +0545

    Fixing error on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header/Header";
-import Hero from "./components/Hero/hero";
+import Hero from "./components/Hero/Hero";
 import "./App.css";
 import Companies from "./components/Companies/Companies";
 import Residencies from "./components/Residencies/Residencies";
